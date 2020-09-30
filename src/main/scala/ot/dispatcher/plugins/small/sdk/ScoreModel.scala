8c5cb7e43b31479b72ae54cae5bca54018c21baf
Dmitriy Arkhireev <darkhireev@isgneuro.com>
2020-09-30 23:35:55 +0300

    The `ScoreModel.score` method `predictionCol` argument type  changed.

--- src/main/scala/ot/dispatcher/plugins/small/sdk/ScoreModel.scala
+++ src/main/scala/ot/dispatcher/plugins/small/sdk/ScoreModel.scala
@@ -4,6 +4,6 @@ import org.apache.spark.sql.DataFrame
 
 trait ScoreModel {
 
-  def score(searchId: Int, labelCol: String, predictionCol: String, metricName: String, featuresNumber: Double): DataFrame => DataFrame
+  def score(searchId: Int, labelCol: String, predictionCol: List[String], metricName: String, featuresNumber: Double): DataFrame => DataFrame
 
 }
